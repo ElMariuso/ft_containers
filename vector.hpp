50dae96b08ba47338c3a8090cabe343641d89207
ElMariuso <thiry.marius@gmail.com>
2022-11-13 00:19:32 +0100

    Member types of vector part 1

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 23:47:52 by root              #+#    #+#             */
-/*   Updated: 2022/11/13 00:02:22 by root             ###   ########.fr       */
+/*   Updated: 2022/11/13 00:18:56 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,24 @@ namespace ft
     class vector
     {
         public:
+
+        /* The first template parameter (T) */
+        typedef T                                           value_type;
+
+        /* The second template parameter (Alloc) */
+        typedef Alloc                                       allocator_type;
+
+        /* allocator_type::reference */
+        typedef typename allocator_type::reference          reference;
+
+        /* allocator_type::const_reference */
+        typedef typename allocator_type::const_reference    const_reference;
+
+        /* allocator_type::pointer */
+        typedef typename allocator_type::pointer            pointer;
+
+        /* allocator_type::const_pointer */
+        typedef typename allocator_type::const_pointer      const_pointer;
 
         private:
     };
