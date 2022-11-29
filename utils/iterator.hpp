1c9dc504ec384d2b1f574bb4b70b144b4c9f609d
ElMariuso <thiry.marius@gmail.com>
2022-11-29 23:04:52 +0100

    Update

--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:56:02 by mthiry            #+#    #+#             */
-/*   Updated: 2022/11/29 21:10:38 by root             ###   ########.fr       */
+/*   Updated: 2022/11/29 21:38:12 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,11 @@ namespace ft
     template <class Iter>
     struct iterator_traits
     {
-        
+        typedef typename Iter::difference_type      difference_type;
+        typedef typename Iter::value_type           value_type;
+        typedef typename Iter::pointer              pointer;
+        typedef typename Iter::reference            reference;
+        typedef typename Iter::iterator_category    iterator_category;
     };
     template <class T>
     struct iterator_traits<T*>
